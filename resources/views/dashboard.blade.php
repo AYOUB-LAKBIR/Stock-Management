dd174e291d9c7a7586441d24554da810d983ebd0
AYOUB LAKBIR <ayoub.lakbir00@gmail.com>
2025-05-24 12:07:24 +0100

    Add Adjustements for Auth

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,16 +11,11 @@
             <a href="{{ route('profile') }}" class="btn btn-primary me-2 float-end">Mon profil</a>
         </div>
         <div class="dashboard-container">
-            <div class="header-actions">
-                <h2>Tableau de bord</h2>
-                
-            </div>
             @if (session('status'))
             <div class="alert alert-success">
                 {{ session('status') }}
             </div>
             @endif
-
             <div class="welcome-card">
                 <h4>Bienvenue, {{ $user->name }} !</h4>
                 <p>Vous êtes maintenant connecté à votre compte.</p>
